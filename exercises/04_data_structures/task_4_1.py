a987d0fd44ab73c964622d02fab3be07f2ac42ea
pyneng <pyneng.course@gmail.com>
2021-06-05 05:21:37 +0000

    Проверены задания 4.1

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -16,5 +16,5 @@
 """
 
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
-result=nat.replace("FastEthernet","GigabitEthernet")
-print (result)
+result = nat.replace("FastEthernet", "GigabitEthernet")
+print(result)
